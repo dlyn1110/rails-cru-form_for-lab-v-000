0ec2281198f99437c6e367836cec85447f014996
Dani Lyn <dlyn1110@gmail.com>
2018-10-19 03:05:30 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,14 +3,14 @@ class GenresController < ApplicationController
   def show
       @genre = Genre.find(params[:id])
   end
- 
+
   def new
-      @genre = Genre.new 
+      @genre = Genre.new
   end
 
   def create
       @genre = Genre.new(genre_params)
-      @genre.save 
+      @genre.save
 
       redirect_to genre_path(@genre)
   end
@@ -18,15 +18,15 @@ class GenresController < ApplicationController
   def edit
       @genre = Genre.find(params[:id])
   end
-  
+
   def update
       @genre = Genre.find(params[:id])
       @genre.update(genre_params)
 
       redirect_to genre_path(@genre)
   end
-  
-  private 
+
+  private
 
   def genre_params
       params.require(:genre).permit(:name)

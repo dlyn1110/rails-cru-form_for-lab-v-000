a3c19dcb3326dcaf64ac0061c243225c6faf3626
Dani Lyn <dlyn1110@gmail.com>
2018-10-19 03:03:56 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -18,14 +18,14 @@ class GenresController < ApplicationController
 	  redirect_to genre_path(@genre)
 	end
 
+  def edit
+	  @genre = Genre.find(params[:id])
+	end
+
 	def update
 	  @genre = Genre.find(params[:id])
 	  @genre.update(genre_params)
 	  redirect_to genre_path(@genre)
-	end
-
-	def edit
-	  @genre = Genre.find(params[:id])
 	end
 
   private

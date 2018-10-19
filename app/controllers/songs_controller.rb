bd548a700ae78d4303c7c9119281d2055d811114
Dani Lyn <dlyn1110@gmail.com>
2018-10-19 03:04:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,14 @@
 class SongsController < ApplicationController
   def index
-       @songs = Song.all 
+       @songs = Song.all
    end
 
    def show
        @song = Song.find(params[:id])
    end
-  
+
    def new
-       @song = Song.new 
+       @song = Song.new
    end
 
    def create
@@ -21,15 +21,15 @@ class SongsController < ApplicationController
    def edit
        @song = Song.find(params[:id])
    end
-   
+
    def update
        @song = Song.find(params[:id])
        @song.update(song_params)
 
        redirect_to song_path(@song)
-   end 
+   end
 
-   private 
+   private
 
    def song_params
        params.require(:song).permit(:name, :artist_id, :genre_id)
